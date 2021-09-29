3f0f09244ba652a5487afa045420b50631a681eb
Andrew Schmidt (CHIP) <anschmidt@microsoft.com>
2021-09-28 23:38:53 -0500

    moved content files

--- functions/content/content.js
+++ functions/content/content.js
@@ -1,14 +1,13 @@
 const CSV = require('comma-separated-values')
 const fs = require('fs')
-const resolve = require('path').resolve
 
 exports.handler = async event => {
     const subject = event.queryStringParameters.name || 'World'
     return {
       statusCode: 200,
       body: JSON.stringify({
-          "Chapter1": CSV.parse(fs.readFileSync(resolve(`./Chapter1.csv`), 'utf-8')),
-          "Chapter2": CSV.parse(fs.readFileSync(resolve(`./Chapter2.csv`), 'utf-8'))
+          "Chapter1": CSV.parse(fs.readFileSync(require.resolve(`./Chapter1.csv`), 'utf-8')),
+          "Chapter2": CSV.parse(fs.readFileSync(require.resolve(`./Chapter2.csv`), 'utf-8'))
       }),
     }
   }
