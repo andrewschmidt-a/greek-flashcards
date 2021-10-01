16dad7852bf1c4480087c2c59e1a3fd48a6c81dc
Andrew Schmidt (CHIP) <anschmidt@microsoft.com>
2021-09-30 22:44:01 -0500

    cleaned up content server

--- functions/content/content.js
+++ functions/content/content.js
@@ -11,13 +11,19 @@ const verifyJwt = NetlifyJwtVerifier({
 exports.handler = verifyJwt(async (event, context) => {
   const { claims } = context.identityContext;
   let path = event.queryStringParameters.path? event.queryStringParameters.path: "index.json";
-  let file = JSON.parse(fs.readFileSync(require.resolve(path)));
+  let fileContents = JSON.parse(fs.readFileSync(require.resolve(path)));
+
+  let returnValue = fileContents.map(el => {
+    if(_.has(el, 'path')){
+      let pathComponents = path.split("/");
+      pathComponents[pathComponents.length-1] = el['path'];
+      return _.merge(el, {"path": components.join(components)});
+    }else{
+      return el;
+    }
+  });
   return {
     statusCode: 200,
-    body: JSON.stringify(file.map(el => {
-      let components = path.split("/");
-      components[components.length-1] = el['path'];
-      return _.merge(el, {"path": components.join(components)});
-    })),
+    body: JSON.stringify(returnValue),
   }
 });
